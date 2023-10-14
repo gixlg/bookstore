0e6d091cb5631290e32e7ee6f1f549d63ca8f419
Luca Giuberti <gix_lg@yahoo.com>
2023-10-14 15:35:37 +0200

    State Pattern - Error revealed

--- src/test/java/it/gixlg/bookstore/AccountTest.java
+++ src/test/java/it/gixlg/bookstore/AccountTest.java
@@ -2,6 +2,7 @@ package it.gixlg.bookstore;
 
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,6 +33,42 @@ class AccountTest {
         account.withdraw();
 
         verify(state).withdraw();
+    }
+
+
+    @Nested
+    class OldTest {
+        @Test
+        void shouldIncreasePointsByDepositingABook() {
+            Account account = new Account(0);
+            account.deposit();
+            assertThat(account.getPoints()).isEqualTo(1);
+        }
+        @Test
+        void shouldGetBonusWhenDepositing() {
+            Account account = new Account(10);
+            account.deposit();
+            assertThat(account.getPoints()).isEqualTo(12);
+        }
+        @Test
+        void shouldDecreasePointsByWithdrawABook() throws WithdrawException {
+            Account account = new Account(3);
+            account.withdraw();
+            assertThat(account.getPoints()).isEqualTo(1);
+        }
+        @Test
+        void shouldWithdrawWithDiscount() throws WithdrawException {
+            Account account = new Account(5);
+            account.withdraw();
+            assertThat(account.getPoints()).isEqualTo(4);
+        }
+        @Test
+        void shouldAvoidToWithdrawWhenThereAreNoMoney() {
+            Account account = new Account(1);
+            ThrowableAssert throwableAssert = assertThatThrownBy(() -> account.withdraw());
+            throwableAssert.isInstanceOf(WithdrawException.class);
+            assertThat(account.getPoints()).isEqualTo(1);
+        }
     }
 
 
