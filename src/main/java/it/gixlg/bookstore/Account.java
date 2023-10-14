ede527ec901a9394f2a921704d09c7c41bec31ea
Luca Giuberti <gix_lg@yahoo.com>
2023-10-14 15:22:17 +0200

    Code works

--- src/main/java/it/gixlg/bookstore/Account.java
+++ src/main/java/it/gixlg/bookstore/Account.java
@@ -5,6 +5,14 @@ import lombok.Getter;
 
 public class Account {
 
+    public static final int THRESHOLD_BONUS = 10;
+    public static final int BONUS = 1;
+    public static final int STANDARD_POINT_FOR_DEPOSIT = 1;
+    public static final int STANDARD_POINTS_FOR_WITHDRAW = 2;
+    public static final int THRESHOLD_DISCOUNT = 5;
+    public static final int DISCOUNT = 1;
+    public static final int THRESHOULD_WITHDRAW = 1;
+
     @Getter(AccessLevel.PROTECTED)
     private int points;
 
@@ -13,10 +21,19 @@ public class Account {
     }
 
     public void deposit() {
-
+        if (points >= THRESHOLD_BONUS) {
+            points = points + BONUS;
+        }
+        points = points + STANDARD_POINT_FOR_DEPOSIT;
     }
 
     public void withdraw() throws WithdrawException {
+        if (points <= THRESHOULD_WITHDRAW) throw new WithdrawException();
+        if (points >= THRESHOLD_DISCOUNT) {
+            points = points + DISCOUNT;
+        }
+        points = points - STANDARD_POINTS_FOR_WITHDRAW;
+
 
     }
 }
--- src/test/java/it/gixlg/bookstore/AccountTest.java
+++ src/test/java/it/gixlg/bookstore/AccountTest.java
@@ -1,9 +1,49 @@
 package it.gixlg.bookstore;
 
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class AccountTest {
+    @Test
+    void shouldIncreasePointsByDepositingABook() {
+        Account account = new Account(0);
+        account.deposit();
+        assertThat(account.getPoints()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldGetBonusWhenDepositing() {
+        Account account = new Account(10);
+        account.deposit();
+        assertThat(account.getPoints()).isEqualTo(12);
+    }
+
+    @Test
+    void shouldDecreasePointsByWithdrawABook() throws WithdrawException {
+        Account account = new Account(3);
+        account.withdraw();
+        assertThat(account.getPoints()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldWithdrawWithDiscount() throws WithdrawException {
+        Account account = new Account(5);
+        account.withdraw();
+        assertThat(account.getPoints()).isEqualTo(4);
+    }
+
+    @Test
+    void shouldAvoidToWithdrawWhenThereAreNoMoney() {
+        Account account = new Account(1);
+
+        ThrowableAssert throwableAssert = assertThatThrownBy(() -> account.withdraw());
+        throwableAssert.isInstanceOf(WithdrawException.class);
+
+        assertThat(account.getPoints()).isEqualTo(1);
+    }
 
 
 }
